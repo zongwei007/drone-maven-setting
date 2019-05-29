597cc770cb775e1747e5ed8d024580bb40a39fb5
Zongwei <zongwei007@gmail.com>
2019-05-29 08:23:47 +0800

    chore: 更新 docker hub 部署配置

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build . -t knives/drone-maven-setting
-docker push knives/drone-maven-setting
+
+curl -H "Content-Type: application/json" \
+  --data '{"source_type": "Branch", "source_name":"master"}' \
+-X POST https://cloud.docker.com/api/build/v1/source/$DOCKER_USER_TOKEN/trigger/$DOCKER_HOOK_TOKEN/call/
